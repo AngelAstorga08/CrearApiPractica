1894839ab19256b4c94d87dcd68c3fad075a4005
AngelAstorga08 <angeledastorga08@gmail.com>
2024-10-09 22:57:59 -0700

    Anadido de cosas

---
+++ API_Capacitacion.DTO/User/CreateUserDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API_Capacitacion.DTO.User
+{
+    public class CreateUserDto
+    {
+        public string Nombres { get; set; }
+        public string Usuario { get; set; }
+        public string Contrasena { get; set; }
+    }
+}
---
+++ API_Capacitacion.DTO/User/UpdateUsuarioDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API_Capacitacion.DTO.User
+{
+    internal class UpdateUsuarioDto
+    {
+        public string Nombres { get; set; }
+        public string Usuario { get; set; }
+        public string Contrasena { get; set; }
+
+    }
+}
